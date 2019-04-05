17ceaf088be1b353c7f3d1e1bf6b4b02c1b18e37
CFBUNDY <48921726+CFBUNDY@users.noreply.github.com>
2019-04-05 19:49:17 -0400

    added rotation

--- main.cc
+++ main.cc
@@ -17,8 +17,19 @@ typedef struct _camera {
     float dir;
 } camera;
 
+xy pointFrom (camera view, xy point) {
+    xy proj; //projected
+    proj.x = point.x - view.p.x;
+    proj.y = point.y - view.p.y;
+    float tx = proj.x*cos(view.dir) - proj.y*sin(view.dir);
+    proj.y = proj.x*sin(view.dir) + proj.y*cos(view.dir);
+    proj.x = tx;
+    return proj;
+} //returns where point appears from view
+
 class Player {
     camera pos;
+    float movespeed = 0.1, turnspeed = 0.005;
     public:
     Player(float x, float y, float dir) {
         pos.p.x = x;
@@ -29,9 +40,9 @@ class Player {
         return pos;
     }
     void movement(float frwd, float side, float turn) {
-        pos.dir += turn;
-        pos.p.x += side;
-        pos.p.y += frwd;
+        pos.dir += turn*turnspeed;
+        pos.p.x += (frwd*sin(pos.dir)+side*cos(pos.dir))*movespeed;
+        pos.p.y += (frwd*cos(pos.dir)-side*sin(pos.dir))*movespeed;
     }
 };
 
@@ -48,11 +59,15 @@ class Cell {
         vector<xy>::iterator it = points.begin();
         xy last = *it;
         for (it++; it != points.end(); it++) {
-            SDL_RenderDrawLine(ren, last.x-c.p.x+(W/2), last.y-c.p.y+(H/2), it->x-c.p.x+(W/2), it->y-c.p.y+(H/2));
+            xy p1 = pointFrom(c, last);
+            xy p2 = pointFrom(c, *it);
+            SDL_RenderDrawLine(ren, p1.x+(W/2), p1.y+(H/2), p2.x+(W/2), p2.y+(H/2));
             last = *it;
         }
         it = points.begin();
-        SDL_RenderDrawLine(ren, last.x-c.p.x+(W/2), last.y-c.p.y+(H/2), it->x-c.p.x+(W/2), it->y-c.p.y+(H/2));
+        xy p1 = pointFrom(c, last);
+        xy p2 = pointFrom(c, *it);
+        SDL_RenderDrawLine(ren, p1.x+(W/2), p1.y+(H/2), p2.x+(W/2), p2.y+(H/2));
     }
 };
 
@@ -77,8 +92,8 @@ int main () {
         SDL_RenderClear(ren);
 
         SDL_SetRenderDrawColor(ren, 255, 255, 255, SDL_ALPHA_OPAQUE);
-        SDL_RenderDrawLine(ren, (W/2)+5, (H/2)-5, (W/2)-5, (H/2)+5);
-        SDL_RenderDrawLine(ren, (W/2)-5, (H/2)-5, (W/2)+5, (H/2)+5);
+        SDL_RenderDrawLine(ren, (W/2)+2, (H/2)-2, (W/2)-2, (H/2)+2);
+        SDL_RenderDrawLine(ren, (W/2)-2, (H/2)-2, (W/2)+2, (H/2)+2);
         singlecell.drawfrom(you.getpos());
 
         SDL_RenderPresent(ren);
@@ -102,7 +117,7 @@ int main () {
                 default: break;
             }
         }
-        you.movement((keys[1]-keys[0])*0.1, (keys[3]-keys[2])*0.1, (keys[5]-keys[4])*0.01);
+        you.movement(keys[1]-keys[0], keys[3]-keys[2], keys[4]-keys[5]);
         SDL_Delay(1);
     }
     return 0;
