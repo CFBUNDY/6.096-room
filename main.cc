2060631a59e9812bcb37682e14f21815d54087b4
CFBUNDY <disobediantpants@gmail.com>
2019-04-17 19:46:56 -0400

    length as fntion, point orientation

--- main.cc
+++ main.cc
@@ -21,6 +21,21 @@ typedef struct _xy {
     }
 } xy;
 
+float pointDistance (xy p1, xy p2) {
+    return sqrt(pow((p1.x-p2.x),2.0)+pow((p1.y-p2.y),2.0));
+}
+
+float dotProduct (xy a, xy b) {
+    return a.x*b.x+a.y*b.y;
+}
+
+// >0 : clockwise
+// =0 : co-linear
+// <0 : counter-clockwise
+float pointOrientation (xy a, xy b, xy c) {
+    return (b.x - a.x)*(c.y - b.y) - (c.x - b.x)*(b.y - a.y);
+}
+
 typedef struct _line {
     xy p1;
     xy p2;
@@ -28,6 +43,9 @@ typedef struct _line {
         : p1(p1), p2(p2) {}
     void draw() {
         SDL_RenderDrawLine(ren, (W/2)+p1.x, (H/2)-p1.y, (W/2)+p2.x, (H/2)-p2.y);
+    }
+    float length() {
+        return pointDistance(p1, p2);
     }
 } line;
 
@@ -44,12 +62,8 @@ xy pointFrom (camera view, xy point) {
     return proj;
 } //returns where point appears from view
 
-float dotProduct (xy a, xy b) {
-    return a.x*b.x+a.y*b.y;
-}
-
 xy closestPointOnSegment (line l, xy p) {
-    float length = sqrt(pow((l.p1.x-l.p2.x),2.0)+pow((l.p1.y-l.p2.y),2.0));
+    float length = l.length();
     float scalar = dotProduct((p-l.p1), (l.p2-l.p1))/length;
     if (scalar < 0) {
         return l.p1;
@@ -108,8 +122,12 @@ class Player {
         for (int i = 0; i < currentCell.cellSize(); i++) {
             line l = currentCell.getSegment(i);
             xy cl = closestPointOnSegment(l, pos.p);
+            if (pointOrientation(l.p1, l.p2, pos.p) > 0) {
+                SDL_SetRenderDrawColor(ren, 255, 0, 0, SDL_ALPHA_OPAQUE);
+            }
             cl = pointFrom(pos, cl);
             SDL_RenderDrawLine(ren, (W/2), (H/2), (W/2)+cl.x, (H/2)-cl.y);
+            SDL_SetRenderDrawColor(ren, 255, 255, 255, SDL_ALPHA_OPAQUE);
         }
     }
 };
@@ -122,6 +140,7 @@ int main () {
     singlecell.addpoint(-60, -60);
     singlecell.addpoint(-80, 20);
     singlecell.addpoint(80, 40);
+    singlecell.addpoint(20, -80);
     Player you(0, 0, 0, singlecell);
     bool keys[6] = {0,0,0,0,0,0};
 
