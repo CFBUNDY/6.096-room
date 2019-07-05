ec4c7287ee9ff10a1b168f6fe39c2dfbc5493ab1
CFBUNDY <48921726+CFBUNDY@users.noreply.github.com>
2019-07-05 16:09:40 -0400

    multiple points, grid draw&snap

--- room-edit/main.cc
+++ room-edit/main.cc
@@ -22,6 +22,10 @@ typedef struct _xy {
     _xy operator-(const _xy& a) {
         return _xy(x-a.x, y-a.y);
     }
+    void round(int g) {
+        x = roundf(x/(float)g)*g;
+        y = roundf(y/(float)g)*g;
+    }
     bool handleColl(const _xy pos) {
         return (pos.x < x+hsz && pos.x > x-hsz && pos.y < y+hsz && pos.y > y-hsz);
     }
@@ -49,6 +53,22 @@ struct _mouse {
     bool l () {return buttonstate & SDL_BUTTON_LMASK;}
 } m;
 
+struct _Map {
+    vector <xy> points;
+    void addPoint(float x, float y) {
+        xy n(x, y);
+        points.push_back(n);
+    }
+    xy getPoint(unsigned int i) {
+        if (i < points.size()) return points[i];
+        xy n(0, 0); return n;
+    }
+    xy * getPointPoint(unsigned int i) {
+        if (i < points.size()) return &points[i];
+        return NULL;
+    }
+} Map;
+
 #undef main
 
 int main () {
@@ -57,7 +77,11 @@ int main () {
     SDL_Window * win = SDL_CreateWindow("Hello?", 160, 120, W, H, SDL_WINDOW_SHOWN);
     ren = SDL_CreateRenderer(win, -1, 0);
 
-    xy cam(0,0), point(0,0), mouse;
+    int grid = 16;
+    xy cam(0,0), mouse;
+    Map.addPoint(0,0);
+    Map.addPoint(40,40);
+    Map.addPoint(40,40);
 
     bool quit = false;
     while (!quit) {
@@ -65,17 +89,52 @@ int main () {
         SDL_RenderClear(ren);
 
         SDL_SetRenderDrawColor(ren, 255, 255, 255, SDL_ALPHA_OPAQUE);
-        if (point.handleColl(m.p+cam)) {
-            if (m.l()) SDL_SetRenderDrawColor(ren, 255, 255, 0, SDL_ALPHA_OPAQUE);
-            else SDL_SetRenderDrawColor(ren, 255, 0, 0, SDL_ALPHA_OPAQUE);
+        //draw grid
+        if (grid != 1) {
+            SDL_SetRenderDrawColor(ren, 48, 48, 48, SDL_ALPHA_OPAQUE);
+            int j = ((W/2)-(int)cam.x)%grid;
+            if (j < 0) j += grid;
+            for (int i = 0; i < (W / grid); i++) {
+                SDL_RenderDrawLine(ren, j, 0, j, H);
+                j += grid;
+            }
+            j = ((H/2)+(int)cam.y)%grid;
+            if (j < 0) j += grid;
+            for (int i = 0; i < (H / grid); i++) {
+                SDL_RenderDrawLine(ren, 0, j, W, j);
+                j += grid;
+            }
+        }{
+            SDL_SetRenderDrawColor(ren, 80, 80, 80, SDL_ALPHA_OPAQUE);
+            int j = ((W/2)-(int)cam.x)%128;
+            if (j < 0) j += 128;
+            for (int i = 0; i < (W / 128); i++) {
+                SDL_RenderDrawLine(ren, j, 0, j, H);
+                j += 128;
+            }
+            j = ((H/2)+(int)cam.y)%128;
+            if (j < 0) j += 128;
+            for (int i = 0; i < (H / 128); i++) {
+                SDL_RenderDrawLine(ren, 0, j, W, j);
+                j += 128;
+            }
         }
-        point.drawHandle(cam);
+        SDL_SetRenderDrawColor(ren, 48, 128, 128, SDL_ALPHA_OPAQUE);
+        SDL_RenderDrawLine(ren, (W/2)-(int)cam.x, 0, (W/2)-(int)cam.x, H);
+        SDL_RenderDrawLine(ren, 0, (H/2)+(int)cam.y, W, (H/2)+(int)cam.y);
+        //draw points
+        SDL_SetRenderDrawColor(ren, 255, 255, 255, SDL_ALPHA_OPAQUE);
+        for (int i = 0; i < Map.points.size(); i++) {
+            Map.getPoint(i).drawHandle(cam);
+        }
 
         //keyboard
         m.update();
         SDL_Event e;
         while (SDL_PollEvent(&e)) {
             switch (e.key.keysym.sym) {
+                case SDLK_RIGHTBRACKET: if (e.type == SDL_KEYDOWN) grid *= 2; break;
+                case SDLK_LEFTBRACKET: if (e.type == SDL_KEYDOWN) if (grid > 1) grid /= 2; break;
                 case SDLK_RETURN:
                 case SDLK_ESCAPE: quit = (e.type == SDL_KEYDOWN); break;
                 default: quit = (e.type == SDL_QUIT);
@@ -87,12 +146,20 @@ int main () {
             if (m.drag) {
                 if (m.grab) {
                     if (m.grab == &cam) cam = cam - (m.p - m.last);
-                    else *m.grab = *m.grab + (m.p - m.last);
+                    else {
+                        *m.grab = m.p + cam;
+                        m.grab->round(grid);
+                    }
                 }
             } else {
-                if (point.handleColl(m.p+cam)) m.grab = &point;
-                else m.grab = &cam;
+                m.grab = &cam;
                 m.drag = true;
+                for (int i = 0; i < Map.points.size(); i++) {
+                    if (Map.getPoint(i).handleColl(m.p+cam)) {
+                        m.grab = Map.getPointPoint(i);
+                        break;
+                    }
+                }
             }
         } else m.drag = m.l();
 
